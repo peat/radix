ecc73df43834a04526b19002f1ccb704f670f7b9
Peat Bakke <peat@peat.org>
2011-08-02 21:47:28 -0700

    Ok, tests passing with bare bones manifest

--- spec/radix_spec.rb
+++ spec/radix_spec.rb
@@ -5,7 +5,7 @@ require File.join( RADIX_ROOT, 'lib', 'radix.rb' )
 describe Radix do
 
   before(:all) do
-    @manifest_schema_path = File.join( RADIX_ROOT, 'schema', 'manifest.xsd' )
+    @schema_path = File.join( RADIX_ROOT, 'schema', 'radix.xsd' )
     @bad_manifest_path = File.join( RADIX_ROOT, 'fixtures', 'manifest-invalid.xml' )
     @good_manifest_path = File.join( RADIX_ROOT, 'fixtures', 'manifest-valid.xml' )
     @private_key_path = File.join( RADIX_ROOT, 'fixtures', 'keys', 'fixture.pem' )
@@ -15,8 +15,8 @@ describe Radix do
   describe "Radix#valid_xml?" do
         
     it "should validate the XML for a given file by enforcing the indicated XSL" do
-      Radix.valid_xml?( @good_manifest_path, @manifest_schema_path ).should be_true
-      Radix.valid_xml?( @bad_manifest_path, @manifest_schema_path ).should be_false
+      Radix.valid_xml?( @good_manifest_path, @schema_path ).should be_true
+      Radix.valid_xml?( @bad_manifest_path, @schema_path ).should be_false
     end
 
   end
@@ -24,8 +24,8 @@ describe Radix do
   describe "Radix#xml_errors" do
     
     it "should provide errors for XML for a given file if it does not match the standard" do
-      Radix.xml_errors( @good_manifest_path, @manifest_schema_path ).length.should eq(0)
-      Radix.xml_errors( @bad_manifest_path, @manifest_schema_path ).length.should eq(2)
+      Radix.xml_errors( @good_manifest_path, @schema_path ).length.should eq(0)
+      Radix.xml_errors( @bad_manifest_path, @schema_path ).length.should eq(2)
     end 
     
   end
