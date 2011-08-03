ab121c76e1d1e5d4733d113268b436bb5d40c16e
Peat Bakke <peat@peat.org>
2011-08-02 19:46:14 -0700

    Renaming methods; more testing

--- spec/radix_spec.rb
+++ spec/radix_spec.rb
@@ -3,26 +3,28 @@ RADIX_ROOT = File.join( File.dirname(__FILE__), '..' )
 require File.join( RADIX_ROOT, 'lib', 'radix.rb' )
 
 describe Radix do
-  
-  describe "Radix#validate_xml" do
-    
-    before(:all) do
-      @manifest_schema_path = File.join( RADIX_ROOT, 'schema', 'manifest.xsd' ) 
-    end
-    
-    it "should validate the XML for a given file by enforcing the indicated XSL" do
-      good_manifest = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'valid.xml' )        
-      Radix.valid_xml?( good_manifest, @manifest_schema_path ).should be_true
 
-      bad_manifest = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'invalid.xml' )        
-      Radix.valid_xml?( bad_manifest, @manifest_schema_path ).should be_false
+  before(:all) do
+    @manifest_schema_path = File.join( RADIX_ROOT, 'schema', 'manifest.xsd' )
+    @bad_manifest_path = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'invalid.xml' )
+    @good_manifest_path = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'valid.xml' )
+  end
+  
+  describe "Radix#xml_errors?" do
+        
+    it "should validate the XML for a given file by enforcing the indicated XSL" do
+      Radix.valid_xml?( @good_manifest_path, @manifest_schema_path ).should be_true
+      Radix.valid_xml?( @bad_manifest_path, @manifest_schema_path ).should be_false
     end
+
+  end
+  
+  describe "Radix#xml_errors" do
     
     it "should provide errors for XML for a given file if it does not match the standard" do
-      bad_manifest = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'invalid.xml' )        
-      Radix.xml_errors( bad_manifest, @manifest_schema_path ).length.should eq(2)
+      Radix.xml_errors( @good_manifest_path, @manifest_schema_path ).length.should eq(0)
+      Radix.xml_errors( @bad_manifest_path, @manifest_schema_path ).length.should eq(2)
     end 
-    
     
   end
   
