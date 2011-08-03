64ab76d9b24bad129718d348de229eb9484dd0d7
Peat Bakke <peat@peat.org>
2011-08-02 19:44:25 -0700

    Renaming methods; more testing

--- lib/radix.rb
+++ lib/radix.rb
@@ -2,11 +2,15 @@ require 'nokogiri'
 
 class Radix
     
-  def self.valid_xml?( xml_path, schema_path )
+  def self.xml_errors( xml_path, schema_path )
     doc = Nokogiri::XML( File.read( xml_path ) )
     xsd = Nokogiri::XML::Schema( File.read( schema_path ) )
     
-    xsd.validate(doc).empty?    
+    xsd.validate(doc)    
+  end
+  
+  def self.valid_xml?( xml_path, schema_path )
+    xml_errors( xml_path, schema_path ).empty?
   end
   
 end
--- spec/radix_spec.rb
+++ spec/radix_spec.rb
@@ -6,14 +6,24 @@ describe Radix do
   
   describe "Radix#validate_xml" do
     
-    it "should validate the XML for a given file by enforcing the indicated XSL" do
-      manifest_valid_path = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'valid.xml' )
-      manifest_schema_path = File.join( RADIX_ROOT, 'schema', 'manifest.xsd' )
-      
-      Radix.valid_xml?( manifest_valid_path, manifest_schema_path ).should be_true
+    before(:all) do
+      @manifest_schema_path = File.join( RADIX_ROOT, 'schema', 'manifest.xsd' ) 
     end
     
-    it "should throw an exception if the XML for a given file does not match the standard"    
+    it "should validate the XML for a given file by enforcing the indicated XSL" do
+      good_manifest = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'valid.xml' )        
+      Radix.valid_xml?( good_manifest, @manifest_schema_path ).should be_true
+
+      bad_manifest = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'invalid.xml' )        
+      Radix.valid_xml?( bad_manifest, @manifest_schema_path ).should be_false
+    end
+    
+    it "should provide errors for XML for a given file if it does not match the standard" do
+      bad_manifest = File.join( RADIX_ROOT, 'fixtures', 'manifest', 'invalid.xml' )        
+      Radix.xml_errors( bad_manifest, @manifest_schema_path ).length.should eq(2)
+    end 
+    
+    
   end
   
 end
